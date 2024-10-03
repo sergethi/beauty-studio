6591f5bd400f63c9c243f388b252bc90e79b46bf
sergethi <nsergearistide@gmail.com>
2024-10-03 17:43:18 -0400

    fixed mobile menu toggle when menu item is clicked

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -12,37 +12,42 @@ import {
   Button,
 } from "@nextui-org/react";
 // import {Link} from "@nextui-org/link";
-import Link from 'next/link';
+import Link from "next/link";
 import logo from "../../public/images/beautyStudio_logo.png";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
-
-
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const pathname = usePathname()
+  const pathname = usePathname();
 
+  const closeMobilMenu = () => {
+    setIsMenuOpen(false);
+  };
+  console.log("isOpen", isMenuOpen);
 
   const menuItems = [
-   { name:"Home", link:"/"},
-    {name:"About", link: "/about"},
-    {name:"Services", link: "/services"},
-    {name:"Pricing", link: "/pricing"},
-    {name:"Contact", link: "/contact"},
-    {name:"Book", link: "/book"},
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    { name: "Services", link: "/services" },
+    { name: "Pricing", link: "/pricing" },
+    { name: "Contact", link: "/contact" },
+    { name: "Book", link: "/book" },
   ];
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen} isBordered shouldHideOnScroll>
+    <Navbar
+      onMenuOpenChange={setIsMenuOpen}
+      isMenuOpen={isMenuOpen}
+      isBordered
+      shouldHideOnScroll
+    >
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
         <NavbarBrand>
-          {/* <AcmeLogo /> */}
-
           <Image src={logo} alt="logo" width={60} height={60} />
 
           <p className="font-bold text-inherit">Jess Beauty Studio</p>
@@ -51,27 +56,47 @@ export default function NavBar() {
 
       <NavbarContent className="hidden sm:flex gap-6" justify="end">
         <NavbarItem>
-          <Link color="foreground" href="/" className={pathname == "/" ? "text-danger": ""}>
+          <Link
+            color="foreground"
+            href="/"
+            className={pathname == "/" ? "text-danger" : ""}
+          >
             Home
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/about" className={pathname == "/about" ? "text-danger": ""} >
+          <Link
+            color="foreground"
+            href="/about"
+            className={pathname == "/about" ? "text-danger" : ""}
+          >
             About
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/services" className={pathname == "/services" ? "text-danger": ""} >
+          <Link
+            color="foreground"
+            href="/services"
+            className={pathname == "/services" ? "text-danger" : ""}
+          >
             Services
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/pricing" className={pathname == "/pricing" ? "text-danger": ""}>
+          <Link
+            color="foreground"
+            href="/pricing"
+            className={pathname == "/pricing" ? "text-danger" : ""}
+          >
             Pricing
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/contact" className={pathname == "/contact" ? "text-danger": ""}>
+          <Link
+            color="foreground"
+            href="/contact"
+            className={pathname == "/contact" ? "text-danger" : ""}
+          >
             Contact
           </Link>
         </NavbarItem>
@@ -81,30 +106,15 @@ export default function NavBar() {
           </Button>
         </NavbarItem>
       </NavbarContent>
-      {/* <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent> */}
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
-            
-              // color={
-              //   pathname === item.link
-              //     ? "danger"
-              //     : "foreground"
-              // }
-              // className={`w-full text-xl`}
-              className={`w-full text-xl ${pathname === item.link ? "text-danger" : "text-foreground"}`}
+              className={`w-full text-xl ${
+                pathname === item.link ? "text-danger" : "text-foreground"
+              }`}
               href={item.link}
-              // size="lg"
+              onClick={closeMobilMenu}
             >
               {item.name}
             </Link>
